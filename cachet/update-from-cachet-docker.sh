71ab6bc35f50f40dc8ccb924d160f636cadee7de
Ted Sluis <ted.sluis@gmail.com>
2018-03-17 10:31:17 +0100

    build cachet nginx alpine arm32v6 base image for raspbian.

---
+++ cachet/update-from-cachet-docker.sh
@@ -0,0 +1,6 @@
+wget -O entrypoint.sh     https://raw.githubusercontent.com/CachetHQ/Docker/master/entrypoint.sh
+wget -O .env.docker       https://raw.githubusercontent.com/CachetHQ/Docker/master/conf/.env.docker
+wget -O nginx-site.conf   https://raw.githubusercontent.com/CachetHQ/Docker/master/conf/nginx-site.conf
+wget -O nginx.conf        https://raw.githubusercontent.com/CachetHQ/Docker/master/conf/nginx.conf
+wget -O php-fpm-pool.conf https://raw.githubusercontent.com/CachetHQ/Docker/master/conf/php-fpm-pool.conf
+wget -O supervisord.conf  https://raw.githubusercontent.com/CachetHQ/Docker/master/conf/supervisord.conf
